2ed58f3da8701c48612a42ed3ba34bdb3645539c
pilarvermu <pilaribermu@gmail.com>
2025-08-22 14:53:52 +0200

    Created core structure

---
+++ webapp/src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import Navbar from "../../components/NavBar/Navbar.jsx"
+import Footer from "../../components/Footer/Footer.jsx"
+export default function Home(){
+return(
+    <>
+    <Navbar/>
+    <Footer/>
+    </>
+)
+}
